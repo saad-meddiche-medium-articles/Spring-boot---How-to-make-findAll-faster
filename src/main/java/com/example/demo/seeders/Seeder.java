4aeacda1e989991fe003c203563928c5beb922e0
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-16 13:28:59 +0100

    feature(backend): add time consumption to Seeder

--- src/main/java/com/example/demo/seeders/Seeder.java
+++ src/main/java/com/example/demo/seeders/Seeder.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 
+import java.util.Date;
+
 @Slf4j
 @Profile("local")
 public abstract class Seeder implements CommandLineRunner {
@@ -11,10 +13,13 @@ public abstract class Seeder implements CommandLineRunner {
     private final String SEED_NAME = this.getClass().getSimpleName().toLowerCase();
 
     @Override
-    final public void run(String... args) throws Exception {
+    final public void run(String... args) {
         log.info("Seeding {} ...", SEED_NAME);
+        Date start = new Date();
         seeding(); // The process of seeding is delegated to the concrete class
+        Date end = new Date();
         log.info("Successfully seeded {}", SEED_NAME);
+        log.info("{} have been seeded in {} ms", SEED_NAME, (end.getTime() - start.getTime()));
     }
 
     protected abstract void seeding();
