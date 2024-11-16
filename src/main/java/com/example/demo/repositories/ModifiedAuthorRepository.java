68ad697b3db1ba3af5aab1164c5b1b3e282e9b29
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-16 12:45:45 +0100

    feature(backend): create repositories

---
+++ src/main/java/com/example/demo/repositories/ModifiedAuthorRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Author;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ModifiedAuthorRepository extends JpaRepository<Author,Long> {
+
+    @EntityGraph(attributePaths = {"books"})
+    List<Author> findAllBy();
+}
