db6e71e1c3e8dbd714a2af8ea23884b383b22ab7
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-16 13:01:53 +0100

    feature(backend): create seeder abstract class

---
+++ src/main/java/com/example/demo/seeders/Seeder.java
@@ -0,0 +1,21 @@
+package com.example.demo.seeders;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+
+@Slf4j
+@Profile("local")
+public abstract class Seeder implements CommandLineRunner {
+
+    private final String SEED_NAME = this.getClass().getSimpleName().toLowerCase();
+
+    @Override
+    final public void run(String... args) throws Exception {
+        log.info("Seeding {} ...", SEED_NAME);
+        seeding(); // The process of seeding is delegated to the concrete class
+        log.info("Successfully seeded {}", SEED_NAME);
+    }
+
+    protected abstract void seeding();
+}
